9c4a8b4ea37e8cf94387e9771831efe0f83b157c
pso247 <p.silva.oria@gmail.com>
2025-04-25 19:57:28 -0500

    Proyecto_inventario

---
+++ ejemplis/ejemplis/FileName.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplis
+{
+    internal class FileName
+    {
+        public class SISTEMA
+        {
+
+            public void Bienvenida()
+            {
+                Console.WriteLine("BIENVENIDO AL SISTEMA DE INVENTARIOS.");
+            }
+            public void AGREGARPRODUCTO()
+            {
+                Console.WriteLine("Producto agregado correctamente.");
+            }
+            public void MOSTRARPRODUCTO()
+            {
+
+            }
+            public void BUSCARPRODUCTO()
+            {
+
+            }
+            public void MODIFICARPRODUCTO()
+            {
+                Console.WriteLine("Producto modificado");
+            }
+            public void ELIMINARPRODUCTO()
+            {
+                Console.WriteLine("Producto eliminado");
+            }
+        }
+    }
+}
+
